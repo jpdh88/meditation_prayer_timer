50d666393eaeabb39202ee6dc3c855b9d8300cdb
jpdh1988 <54423825+jpdh88@users.noreply.github.com>
2020-02-06 11:43:53 -0500

    Edits SubSequence class
    toString method formatting

--- java/src/Sequence.java
+++ java/src/Sequence.java
@@ -1,6 +1,46 @@
+import java.util.ArrayList;
+
 /**
  * Represents a Sequence object. This object will be used to control a meditation session.
  */
 public class Sequence {
+    // VARIABLES
+    ArrayList<SubSequence> subSequences = new ArrayList<SubSequence>();
 
+    // METHODS
+    // *** Constructors
+    /**
+     * Empty constructor: Creates a default sequence
+     */
+    public Sequence() {
+        subSequences.add( new SubSequence("first"));
+        subSequences.add( new SubSequence("last"));
+    }
+
+    // *** Variable methods
+    public void addSubSequence(int duration) {
+        subSequences.add(subSequences.size() - 1, new SubSequence(duration));
+    }
+
+    // *** Utility methods
+    /**
+     * to String method
+     * @return A representation of the object in String form
+     */
+    @Override
+    public String toString() {
+        String output = "Sequence Object:";
+        for (SubSequence item: subSequences) {
+            output += "\n - " + item;
+        }
+        return output;
+    }
+
+    // MAIN METHOD FOR TESTING
+    public static void main(String[] args) {
+        Sequence my_sequence = new Sequence();
+        my_sequence.addSubSequence(500);
+        my_sequence.addSubSequence(100);
+        System.out.println(my_sequence);
+    }
 }
