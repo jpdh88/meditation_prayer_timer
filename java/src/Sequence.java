a6e2db425e30cef62cb53217a14689f47fde6e36
jpdh1988 <54423825+jpdh88@users.noreply.github.com>
2020-02-07 11:16:44 -0500

    Fixes bugs in Sequence class

--- java/src/Sequence.java
+++ java/src/Sequence.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
  *  - Essentially, a Sequence object will be an array of SubSequences.
  *  - The first SubSequence must use the primary sound
  *  - The last SubSequence must use the primary sound and be of length 0
+ *  - Indexing arrays in this object follows the Java default (beginning at 0), so user interface must be formatted
+ *      accordingly (by subtracting 1 before it is passed to any Sequence method)
  * @author Joseph Haley
  */
 public class Sequence {
@@ -22,7 +24,7 @@ public class Sequence {
         subSequenceArray.add( new SubSequence("last"));
     }
 
-    // *** Variable methods
+    // *** Values methods
 
     /**
      * Get the SubSequenceArray
@@ -31,12 +33,22 @@ public class Sequence {
     public ArrayList getSequenceArray() {
         return subSequenceArray;
     }
+
+    /**
+     * Get the number of SubSequences in the array
+     * @return The number of SubSequences in the array
+     */
+    public int getNumSubSequences() {
+        return subSequenceArray.size();
+    }
+
+    // *** Manipulation methods
     /**
      * Creates a new SubSequence and inserts it at index location in subSequenceArray
      * @param index The location in subSequenceArray Where the new SubSequence is to be added
      * @param duration The length of the new SubSequence
      */
-    public void addSubSequence(int index, int duration) {
+    public void addSubSequence(int duration, int index) {
         subSequenceArray.add(index, new SubSequence(duration));
     }
     /**
@@ -44,7 +56,21 @@ public class Sequence {
      * @param duration The duration of the new SubSequence
      */
     public void addSubSequence(int duration) {
-        this.addSubSequence(subSequenceArray.size() - 1, duration);
+        this.addSubSequence(duration, subSequenceArray.size() - 1);
+    }
+
+    /**
+     * Edit the duration of a particular SubSequence in the subSequenceArray
+     *  - The last SubSequence in the array cannot be changed
+     * @param index The index of the Subsequence in subSequenceArray that is being being edited
+     * @param newDuration The new duration of the SubSequence
+     */
+    public void editSubSequence (int index, int newDuration) {
+        if (index < subSequenceArray.size() - 1) {
+            subSequenceArray.get(index).setDuration(newDuration);
+        } else {
+            System.out.println("SubSequence Class Error: You can't edit that SubSequence");
+        }
     }
 
     /**
@@ -61,24 +87,44 @@ public class Sequence {
 
     }
 
-    /**
-     * Edit the duration of a particular SubSequence in the subSequenceArray
-     *  - The last SubSequence in the array cannot be changed
-     * @param index The index of the Subsequence in subSequenceArray that is being being edited
-     * @param newDuration The new duration of the SubSequence
-     */
-    public void editSubSequence (int index, int newDuration) {
-        if (index < subSequenceArray.size() - 1) {
-            subSequenceArray.get(index).setDuration(newDuration);
-        } else {
-            System.out.println("You cannot edit that SubSequence");
-        }
-    }
-
     // *** Utility methods
     /**
+     * Returns a visual representation of a Sequence object in a String
+     * @return Visual representation of the Sequence (in String form)
+     */
+    public String drawSequenceLine () {
+        String line1 = "| Your session:\t\t";
+        String line2 = "| SubSequence #:\t";
+        /** ArrayList of SubSequence objects **/
+        ArrayList<SubSequence> subSequenceArrayList = new ArrayList<>(this.getSequenceArray());
+        /** Convert ArrayList into an Array **/
+        SubSequence[] subSequenceArray = subSequenceArrayList.toArray(new SubSequence[subSequenceArrayList.size()]);
+
+        int counter = 1;
+        for (SubSequence item: subSequenceArray) { // iterate through each member of the array
+            int sound = item.getSound(); // the SubSequence's sound
+            int duration = item.getDuration(); // the SubSequence's duration
+            String dashes = new String(new char[(duration / 60)]).replace("\0", "-");
+            String spaces = new String(new char[(duration / 60)]).replace("\0", " ");
+
+            switch(sound) {
+                case 0:
+                    line1 += "O" + dashes;
+                    line2 += counter + spaces;
+                    break;
+                case 1:
+                    line1 += "o" + dashes;
+                    line2 += counter + spaces;
+                    break;
+            }
+            counter++;
+
+        }
+        return line1 + "\n" + line2;
+    }
+    /**
      * toString method
-     * @return A representation of the object in String form
+     * @return Returns a String of the Object's variables
      */
     @Override
     public String toString() {
