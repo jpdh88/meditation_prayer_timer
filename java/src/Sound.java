fca79d993b949a85d2dc14e5b29e8afa43f013e5
jpdh1988 <54423825+jpdh88@users.noreply.github.com>
2020-02-08 15:17:49 -0500

    Creates Sound object and a lot of its code

---
+++ java/src/Sound.java
@@ -0,0 +1,136 @@
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Represents a Sound object. A Sound object stores the sound to be used in a SubSequence, information related to that
+ * sound, a list of all the available sounds, etc.
+ *
+ * @author Joseph Haley
+ */
+public class Sound {
+    // VARIABLES
+    /** Directory where all the sounds are located **/
+    private String soundsDir = "src/resources/sounds/";
+    /** List of sounds available to the user **/
+    private Map<String, String> soundList = new HashMap<String, String>(); // this map is populated in the Constructor
+    /** The name of the sound chosen for this Sound object **/
+    private String soundName; // represents a key in the soundList Map
+    /** The AudioInputStream object of the chosen sound **/
+    private AudioInputStream soundStream;
+    /** The AudioFormat object of the chosen sound **/
+    private AudioFormat soundStreamFormat; // for getting information about the sound file
+
+
+    // METHODS
+    // *** Constructors
+    /**
+     * Constructor method
+     * @param soundName the name of the sound chosen for this Sound object (must be one of the keys in the soundList Map
+     */
+    public Sound(String soundName) {
+        soundList.put("Meditation Bell 1", soundsDir + "meditation_bell_low.wav");
+        soundList.put("Meditation Bell 2", soundsDir + "meditation_bell_high.wav");
+        soundList.put("Church Bell 1", soundsDir + "church_bell_low.wav");
+        soundList.put("Church Bell 2", soundsDir + "church_bell_high.wav");
+        soundList.put("Church Bell 3", soundsDir + "church_bell_low_2.wav");
+        soundList.put("Church Bells", soundsDir + "pealing_bells.wav");
+        soundList.put("Birds", soundsDir + "birds.wav");
+        soundList.put("Seashore", soundsDir + "seashore.wav");
+        soundList.put("Cafe", soundsDir + "cafe.wav");
+        soundList.put("Gibberish", soundsDir + "gibberish.wav");
+        setSound(soundName);
+    }
+
+    // *** Values Methods
+    /**
+     * Sets the chosen sound: calls
+     */
+    public void setSound(String soundName) {
+        if (soundList.containsKey(soundName)) {
+            setSoundName(soundName);
+            try {
+                setSoundStream(soundName);
+            } catch (Exception e) {
+                System.out.println("Sound Class Exception in setSound method\n\t" + e);
+            }
+        } else {
+            System.out.println("Sound Class Error: that sound does not exist.");
+        }
+    }
+
+    /**
+     * Sets the soundName according to the sound chosen by setSound
+     * @param soundName The name of the sound file chosen
+     */
+    private void setSoundName(String soundName) {
+        if (soundList.containsKey(soundName)) {
+            this.soundName = soundName;
+        } else {
+            System.out.println("Sound Class Error: that sound name does not exist in the list of sounds.");
+        }
+    }
+    /**
+     * Gets the chosen sound's name
+     * @return The name of the chosen sound
+     */
+    public String getSoundName() {
+        return soundName;
+    }
+    /**
+     * Sets the soundStream according to the soundName given
+     * @param soundName
+     */
+    private void setSoundStream(String soundName) throws Exception {
+        try {
+            File soundFile = new File(soundList.get(soundName));
+            soundStream = AudioSystem.getAudioInputStream(soundFile);
+            soundStreamFormat = soundStream.getFormat();
+        } catch (Exception e) {
+            System.out.println("Sound Class Exception in setSoundStream method\n\t" + e);
+        }
+    }
+
+    /**
+     * Gets the soundStream
+     * @return the soundStream AudioInputStream object
+     */
+    public AudioInputStream getSoundStream() {
+        return soundStream;
+    }
+    /**
+     * Get the names of the available sounds
+     * @return A Set containing the names of available sounds
+     */
+    public Set getSoundList() {
+        return soundList.keySet();
+    }
+
+    /**
+     * Gets the duration of the chosen sound
+     * @return Duration of the sound in seconds
+     */
+    public double getSoundDuration() {
+        long frames = soundStream.getFrameLength();
+        double soundDuration = (frames+0.0) / soundStreamFormat.getFrameRate();
+
+        return soundDuration;
+    }
+
+    /**
+     * MAIN FUNCTION FOR TESTING
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("---------------TESTING----------------");
+        Sound mySound = new Sound("Birds");
+        System.out.println(mySound.getSoundList());
+        System.out.println(mySound.getSoundName());
+        System.out.println(mySound.getSoundStream());
+        System.out.println(mySound.getSoundDuration());
+    }
+}
