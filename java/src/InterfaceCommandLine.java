ab77faae902f20ad7a0477c1b6c8f92dbbb6a5bf
jpdh1988 <54423825+jpdh88@users.noreply.github.com>
2020-02-08 11:48:04 -0500

    Adds several minor features
    Adds consistent error messages (using a method)
    Adds consistent prompt messages (using a method)
    Adds more data validation for user interface
    Probably some other minor tweaks

--- java/src/InterfaceCommandLine.java
+++ java/src/InterfaceCommandLine.java
@@ -12,6 +12,46 @@ public class InterfaceCommandLine {
     private static Scanner keybIn = new Scanner(System.in);
 
     /**
+     * Method for error messages: ensures a consistent format for error messages
+     * @param message The error message to be displayed to the user
+     * @return The error message to be displayed to the user
+     */
+    public static String errorMsg(String message) {
+        return  "\toops[" +
+                message +
+                "]";
+    }
+
+    /**
+     * Method for prompts: ensures a consistent format for prompt messages based on the menu level they're at
+     * @param message The prompt message to be displayed to the user
+     * @param level The level of the menu that the prompt is in
+     * @return The formatted prompt message
+     */
+    public static String prompt(String message, int level) {
+        final String levelSymbol = "+";
+        String levelIndicator = "";
+
+        switch (level) {
+            case 1:
+                levelIndicator = levelSymbol;
+                break;
+            case 2:
+                levelIndicator = levelSymbol + levelSymbol;
+                break;
+            case 3:
+                levelIndicator = levelSymbol + levelSymbol + levelSymbol;
+                break;
+            case 4:
+                levelIndicator = levelSymbol + levelSymbol + levelSymbol;
+                break;
+            default:
+                System.out.println("InterfaceCommandLine error: programmer should add another level to prompt method");
+        }
+        return levelIndicator + " " + message;
+    }
+
+    /**
      * Method for editing a meditation session (i.e. a Sequence object)
      * Presents and controls the command line user interface
      */
@@ -21,7 +61,7 @@ public class InterfaceCommandLine {
             System.out.println("+--------------------------------------------------");
             System.out.println(userSequence.drawSequenceLine());
             System.out.println("+--------------------------------------------------");
-            System.out.println("++ What would you like to do?");
+            System.out.println(prompt("What would you like to do?", 2));
             System.out.println("\t1) ADD a SubSequence to the end");
             System.out.println("\t2) ADD a SubSequence at a location");
             System.out.println("\t3) EDIT a SubSequence");
@@ -39,11 +79,11 @@ public class InterfaceCommandLine {
                 case 1: // Add at end
                     boolean doneCase1 = false;
                     while (!doneCase1) {
-                        System.out.println("+++ ADD - How many seconds do you want it to be?");
+                        System.out.println(prompt("ADD - How many seconds do you want it to be?", 3));
                         int duration = keybIn.nextInt();
 
                         if (duration < 0) {
-                            System.out.println("x[Duration must be 0 or greater.]");
+                            System.out.println(errorMsg("Duration must be 0 or greater."));
                         } else {
                             userSequence.addSubSequence(duration);
                             doneCase1 = true;
@@ -53,9 +93,9 @@ public class InterfaceCommandLine {
                 case 2: // Add at location
                     boolean doneCase2 = false;
                     while (!doneCase2) {
-                        System.out.println("+++ ADD - How many seconds do you want it to be?");
+                        System.out.println(prompt("ADD - How many seconds do you want it to be?", 3));
                         int duration2 = keybIn.nextInt();
-                        System.out.println("+++ ADD - At what location?");
+                        System.out.println(prompt("ADD - At what location?", 3));
                         int location = keybIn.nextInt();
 
                         int numSubSequences = userSequence.getNumSubSequences();
@@ -64,7 +104,7 @@ public class InterfaceCommandLine {
                                 doneCase2 = true;
                                 break;
                             case 1:
-                                System.out.println("x[You can't add a sequence before the first one.]");
+                                System.out.println(errorMsg("You can't add a sequence before the first one"));
                                 break;
                             default:
                                 if (location > numSubSequences) {
@@ -79,18 +119,18 @@ public class InterfaceCommandLine {
                 case 3: // Edit
                     boolean doneCase3 = false;
                     while (!doneCase3) {
-                        System.out.println("+++ EDIT - What SubSequence would you like to edit?");
+                        System.out.println(prompt("EDIT - What SubSequence would you like to edit?", 3));
                         int whichSubSequence = keybIn.nextInt();
 
                         int numSubSequences = userSequence.getNumSubSequences();
                         if (whichSubSequence == numSubSequences) {
-                            System.out.println("x[You can't edit the final sequence.]");
+                            System.out.println(errorMsg("You can't edit the final sequence."));
                         } else if (whichSubSequence == 0) {
                             doneCase3 = true;
                         } else if (whichSubSequence < 0 || whichSubSequence > numSubSequences) {
-                            System.out.println("x[Invalid choice.]");
+                            System.out.println(errorMsg("Invalid choice."));
                         } else {
-                            System.out.println("+++ EDIT - What new duration do you want to give it?");
+                            System.out.println(prompt("EDIT - What new duration do you want to give it?", 3));
                             int duration3 = keybIn.nextInt();
 
                             userSequence.editSubSequence(whichSubSequence - 1, duration3);
@@ -99,37 +139,52 @@ public class InterfaceCommandLine {
                     }
                     break;
                 case 4: // Swap
-                    boolean doneCase4 = false;
-                    while (!doneCase4) {
-                        System.out.println("+++ SWAP - Which are the two SubSequences you would like to swap?");
-                        int swapSequence1 = keybIn.nextInt();
-                        int swapSequence2 = keybIn.nextInt();
+                    switch (userSequence.getNumSubSequences()) {
+                        case 2:
+                        case 3:
+                            System.out.println(errorMsg("There are not enough SubSequences for SWAP."));
+                            break;
+                        default:
+                            boolean doneCase4 = false;
+                            while (!doneCase4) {
+                                System.out.println(prompt("SWAP - Which are the two SubSequences you would like to swap?", 3));
+                                int swapSequence1 = keybIn.nextInt();
+                                int swapSequence2 = keybIn.nextInt();
 
-                        if (swapSequence1 == 0 || swapSequence2 == 0) {
-                            doneCase4 = true;
-                        } else {
-                            userSequence.swapSubSequences(swapSequence1 - 1, swapSequence2 - 1);
-                            doneCase4 = true;
-                        }
+                                if (swapSequence1 == 0 || swapSequence2 == 0) {
+                                    doneCase4 = true;
+                                } else {
+                                    userSequence.swapSubSequences(swapSequence1 - 1, swapSequence2 - 1);
+                                    doneCase4 = true;
+                                }
+                            }
+                            break;
                     }
                     break;
                 case 5: // DELETE
-                    boolean doneCase5 = false;
-                    while (!doneCase5) {
-                        System.out.println("+++ DELETE - Which SubSequence would you like to delete?");
-                        int whichSubSequence = keybIn.nextInt();
+                    switch (userSequence.getNumSubSequences()) {
+                        case 2:
+                            System.out.println(errorMsg("There are no SubSequences to DELETE."));
+                            break;
+                        default:
+                            boolean doneCase5 = false;
+                            while (!doneCase5) {
+                                System.out.println(prompt("DELETE - Which SubSequence would you like to delete?", 3));
+                                int whichSubSequence = keybIn.nextInt();
 
-                        if (whichSubSequence == 0){
-                            doneCase5 = true;
-                        }  else if (whichSubSequence > userSequence.getNumSubSequences()) {
-                            System.out.println("x[Sequence Class Error: Invalid index.]");
-                        } else if (whichSubSequence - 1 == 0 || whichSubSequence == userSequence.getNumSubSequences()){
-                            System.out.println("x[Sequence Class Error: You can't remove the first or last sequence.]");
-                        } else {
-                            userSequence.deleteSubSequence(whichSubSequence - 1);
-                            doneCase5 = true;
-                        }
+                                if (whichSubSequence == 0) {
+                                    doneCase5 = true;
+                                } else if (whichSubSequence < 0 || whichSubSequence > userSequence.getNumSubSequences()) {
+                                    System.out.println(errorMsg("Invalid choice."));
+                                } else if (whichSubSequence == 1 || whichSubSequence == userSequence.getNumSubSequences()) {
+                                    System.out.println(errorMsg("You can't remove the first or last sequence."));
+                                } else {
+                                    userSequence.deleteSubSequence(whichSubSequence - 1);
+                                    doneCase5 = true;
+                                }
+                            }
                     }
+                    break;
             }
         }
     }
@@ -143,7 +198,7 @@ public class InterfaceCommandLine {
         boolean doneProgram = false;
 
         while (!doneProgram) {
-            System.out.println("+ What would you like to do? (Enter 0 at any time to exit)");
+            System.out.println(prompt("What would you like to do? (Enter 0 at any time to exit)", 1));
             System.out.println("\t1) See my session");
             System.out.println("\t2) Edit my session");
             int userChoice = keybIn.nextInt();
@@ -153,12 +208,14 @@ public class InterfaceCommandLine {
                     doneProgram = true;
                     break;
                 case 1:
+                    System.out.println("+--------------------------------------------------");
                     System.out.println(userSequence.drawSequenceLine());
+                    System.out.println("+--------------------------------------------------");
                     break;
                 case 2:
                     editSequence();
                 default:
-                    System.out.println("[- Incorrect choice.]");
+                    System.out.println("[- Invalid choice.]");
             }
         }
 
