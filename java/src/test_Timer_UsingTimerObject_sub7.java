7d40da5ebaa1a0f72e29a6dedb19c978e6d4a1ea
jpdh1988 <54423825+jpdh88@users.noreply.github.com>
2020-02-15 22:23:55 -0500

    Adds new class (still in development)

--- java/src/test_Timer_UsingTimerObject_sub7.java
+++ java/src/test_Timer_UsingTimerObject_sub7.java
@@ -1,202 +1,135 @@
-/**
- * https://www.geeksforgeeks.org/java-util-timer-class-java/
- */
-
-/**
- * https://www.geeksforgeeks.org/java-util-timertask-class-java/
- * with synchronisation
- */
-
-/**
- * https://docs.oracle.com/javase/7/docs/api/java/util/Timer.html#schedule(java.util.TimerTask,%20java.util.Date)
- */
-
-/**
- * The general idea here:
- *  - To schedule one task: TIMER.schedule(task, delay before play sound--long)
- *  - What I have to do:
- *      .schedule(play Interval[0] sound, 0 delay)
- *      .schedule(play Interval[1] sound, Interval[0] delay)
- *      .schedule(play Interval[2] sound, Interval[1] delay)
- *      ...etc.
- *      .schedule(play Interval[last] sound, Interval[last - 1] delay)
- */
-
 import javax.sound.sampled.AudioInputStream;
 import java.util.*;
 
-// USING THIS METHOD FOR THE MEDITATION AND PRAYER TIMER !!!!!!!!!!
+/**
+ * A method for running a Sequence. Essentially, it parses a Sequence into TimerTasks which are scheduled in a Timer
+ *  object running as a daemon.
+ *
+ * @author Joseph Haley
+ *
+ * TODO:
+ *  1) output running status of timer to console
+ *      - have another thread for this?
+ *  2) ensure that when Timer tasks are finished the while loop checking for user input also ends
+ *      - put the while loop in its own thread?
+ *  3) a method to pause the timer
+ */
+class SoundTask extends TimerTask {
+    // VARIABLES
+    /** Info: oneMin = 60s * 1000ms **/
+    private int duration; // in milliseconds
+    private AudioInputStream sound;
 
-// CAN JUST SET isDaemon TO TRUE IN THE TIMER CONSTRUCTOR AND IT RUNS THE TIMER IN ANOTHER THREAD!!!!!!!
+    // METHODS
+    // *** Constructors
+    public SoundTask (AudioInputStream sound) {
+        this.sound = sound;
+    }
+
+    // *** Utility Methods
+    /**
+     * When a SoundTask object is called, the following code is run
+     */
+    public void run() {
+        try {
+            Sound.playSound(sound);
+            System.out.println("Sound played.");
+        } catch (Exception e) {
+            System.out.println("SoundTask Object Error: " + e);
+        }
+    }
+
+}
+
 public class test_Timer_UsingTimerObject_sub7 {
+    // VARIABLES
+    private long startTime; // in milliseconds
 
-    public static class TimeDurationTask extends TimerTask {
-        // VARIABLES
-        /** Info: oneMin = 60s * 1000ms **/
-        private int duration; // in milliseconds
+    private Sequence sequence;
+    private Timer timer;
 
-        // METHODS
-        // *** Constructors
-        public TimeDurationTask (int duration) {this.duration = duration * 1000;}
+    // METHODS
+    // *** Constructor(s)
 
-        // *** Utility Methods
-        public void run() {
-            long startTime = System.currentTimeMillis();
-            long elapsedTime = 0L;
-
-            int seconds = 16;
-            boolean hasDisplayed = false;
-            long lastSecond = 0L;
-            while (elapsedTime <= duration) {
-                if (elapsedTime % 1000 == 0) { // print something every second
-                    if (hasDisplayed == false) {
-                        System.out.println(seconds);
-                        seconds++;
-                        hasDisplayed = true;
-                    }
-                }
-                if (lastSecond + 1000 == elapsedTime) {
-                    hasDisplayed = false;
-                    lastSecond += 1000;
-                }
-                elapsedTime = (new Date()).getTime() - startTime;
-            }
-        }
+    // *** Value Method(s)
+    private void setStartTime() {
+        this.startTime = System.currentTimeMillis();;
+    }
+    public long getStartTime() {
+        return startTime;
+    }
+    public long getElapsedTime() {
+        return ( (new Date()).getTime() - startTime);
     }
 
-    public static class PlaySoundTask extends TimerTask {
-        // VARIABLES
-        private AudioInputStream sound;
-
-        // METHODS
-        // *** Constructors
-        /**
-         * Constructor method: Creates a task that will play a sound
-         * @param sound The sound that will be played
-         */
-        public PlaySoundTask (AudioInputStream sound) {
-            this.sound = sound;
-        }
-
-        // *** Utility methods
-        /**
-         * The code that is run by the Timer
-         */
-        public void run() {
-            try {
-                Sound.playSound(sound);
-                System.out.println("Sound played.");
-            } catch (Exception e) {
-                System.out.println("EROROOPROROR");
-            }
-        }
+    // *** Utility Method(s)
+    public void createTimer(Sequence sequence) {
+        this.sequence = sequence;
+        timer = new Timer("Timer Daemon", true);
     }
 
-    public static void main(String[] args)
-    {
-        Scanner keybIn = new Scanner(System.in);
-        Timer timer = new Timer(true);
-        Sequence sequence = new Sequence();
-        sequence.editInterval(0, 20);
-        sequence.addInterval(30);
-        sequence.addInterval(20);
-        // Should be: Tone - 5s - Tone - 10s - Tone - 5s - Tone
-
-        // Sequence to array of Intervals
+    /**
+     * Method that runs the timer. Essentially, it passes tasks to the Timer which starts running them.
+     */
+    public void runTimer() {
+        // Get Sequence intervals into an Array of intervals
         /** ArrayList of Interval objects **/
         ArrayList<Interval> intervalArrayList = new ArrayList<>(sequence.getSequenceArray());
         /** Convert ArrayList into an Array **/
         Interval[] intervalArray = intervalArrayList.toArray(new Interval[intervalArrayList.size()]);
-        /**
-         * TimerTask array = we will build tasks into this
-         *  - General structure:
-         *      task:   O: play Main sound
-         *      task:   -: timer - length of sound (15s)
-         *      task:   o: play Secondary sound
-         *      task:   -: timer - length of sound (15s)
-         *      task:   o: play Secondary sound
-         *      task:   -: timer - length of sound (15s)
-         *      etc...
-         *      task:   O: play Main sound
-         *  - Therefore, length of TimerTask array will be length(Interval array)*2 - 1
-         */
-        TimerTask[] timerTasks = new TimerTask[(intervalArray.length * 2) - 1];
 
-        // Build an array of tasks to be performed by the timer
-        //  - 1) first Interval (index 0 of array)
-        //      Task: Play a sound
-        try {
-            String soundPath = Sound.getPathFromSoundList(sequence.getMainSound());
-            timerTasks[0] = new PlaySoundTask(Sound.createSoundStream(soundPath));
-        } catch (Exception e) {
-            System.out.println("Problem 1");
-        }
-        //      Task: Duration of silence
-        timerTasks[1] = new TimeDurationTask(intervalArray[0].getDuration() - Sound.clipDuration);
-
-        //  - 2) middle intervals
-        int intervalIndex;
-        int taskIndex = 2;
-        for (intervalIndex = 1; intervalIndex <= intervalArray.length - 2; intervalIndex++) {
-            // Task: Play a sound
-            try {
-                String soundPath = Sound.getPathFromSoundList(sequence.getIntervalSound());
-                timerTasks[taskIndex] = new PlaySoundTask(Sound.createSoundStream(soundPath));
-            } catch (Exception e) {
-                System.out.println("Problem 2");
-            }
-            // Task: Duration of silence
-            timerTasks[taskIndex + 1] = new TimeDurationTask(intervalArray[intervalIndex].getDuration() - Sound.clipDuration);
-            taskIndex += 2;
-        }
-
-        //  - 3) final interval
-        try {
-            String soundPath = Sound.getPathFromSoundList(sequence.getMainSound());
-            timerTasks[timerTasks.length - 1] = new PlaySoundTask(Sound.createSoundStream(soundPath));
-        } catch (Exception e) {
-            System.out.println("Problem 3");
-        }
-
-        for (TimerTask task: timerTasks) {
-            System.out.println("Tasks: " + task);
-        }
-
-        // Add the above tasks to the Timer with appropriate duration values
-        System.out.println("Num timer tasks: " + timerTasks.length);
+        String soundPath = "";
         long accruedDuration = 1L;
-        intervalIndex = 0;
-        for (taskIndex = 0; taskIndex < timerTasks.length - 1; taskIndex += 2) { // takes care of all but final task
-            System.out.println("accruedDuration: " + accruedDuration);
-            System.out.println("taskIndex: " + taskIndex);
-            System.out.println("\ttwo tasks");
-            timer.schedule(timerTasks[taskIndex], accruedDuration);
-            timer.schedule(timerTasks[taskIndex + 1], accruedDuration + Sound.clipDuration);
-            // we delay the second task by the length of the sound clip so that it only starts after the sound has played
-            //  we can do this without losing total time because we subtracted the same amount when we built the task array
+        for (int intervalIndex = 0; intervalIndex < intervalArray.length; intervalIndex++) {
+            if (intervalIndex == 0) { // First interval (uses main sound)
+                soundPath = Sound.getPathFromSoundList(sequence.getMainSound());
+
+                this.setStartTime(); // "Start" the object's internal status clock (to check how long timer has run)
+            } else if (intervalIndex == intervalArray.length - 1) { // Last interval (uses main sound)
+                soundPath = Sound.getPathFromSoundList(sequence.getMainSound());
+            } else { // Middle intervals (use secondary sound)
+                soundPath = Sound.getPathFromSoundList(sequence.getIntervalSound());
+            }
+
+            try {
+                timer.schedule(new SoundTask(Sound.createSoundStream(soundPath)), accruedDuration * 1000);
+            } catch (Exception e) {
+                System.out.println("Exception: " + e);
+            }
 
             accruedDuration += intervalArray[intervalIndex].getDuration();
-            intervalIndex++;
         }
-        System.out.println("accruedDuration: " + accruedDuration);
-        timer.schedule(timerTasks[taskIndex], accruedDuration); // the final task
+    }
+    public void endTimer() {
+        timer.cancel();
+    }
+    public void pauseTimer() {
 
-        // WORKS!!!!!
-//        try {
-//            Thread.sleep(6000);
-//        } catch (Exception e) {
-//            System.out.println(e);
-//        }
-//        timer.cancel();
+    }
 
-        // WORKS!!!!!!!!!
-        boolean done = false;
-        while (!done) {
-            int exitNum = keybIn.nextInt();
-            if (exitNum == 0) {
-                done = true;
-                timer.cancel();
+    /**
+     * Main Method: For testing
+     * @param args
+     */
+    public static void main(String[] args) {
+        test_Timer_UsingTimerObject_sub7 sequenceTimer = new test_Timer_UsingTimerObject_sub7();
+        Sequence sequence = new Sequence();
+        sequence.editInterval(0, 20);
+        sequence.addInterval(30);
+        sequence.addInterval(20);
+
+        sequenceTimer.createTimer(sequence);
+        sequenceTimer.runTimer();
+
+        while (true) {
+            Scanner keybIn = new Scanner(System.in);
+            String userInput = keybIn.nextLine();
+
+            if (userInput.equals("q")) { // exit condition is met
+                sequenceTimer.endTimer();
+                break;
             }
         }
     }
+
 }
