b19df05fb2dcab5c27e787e721a7cc15606d72be
jpdh1988 <54423825+jpdh88@users.noreply.github.com>
2020-03-06 15:10:29 -0500

    Brings variables I think we'll need from SequenceTimer_old

--- java/src/SequenceTimer.java
+++ java/src/SequenceTimer.java
@@ -1,11 +1,31 @@
+import java.util.Timer;
+
 /**
  * stuff
  * started 2020-03-06
+ * TODO:
+ *  - variables
+ *  - methods
+ *      -
  *
  * @author josephhaley
  */
 public class SequenceTimer {
     // VARIABLES
+    /** The time when the first SoundTask is assigned to the Timer--effectively, this is the start time of the Timer **/
+    private long startTime; // in milliseconds
+    /** The total duration that the Timer will run for **/
+    private long totalDuration; // in milliseconds
+
+    /** The Sequence that will be parsed into tasks and then passed to the timer to be run **/
+    private Sequence sequence;
+
+    /** For communication between threads **/
+    private boolean isTimerRunning = false; // for indicating whether the Timer is running
+    private boolean killStatusThread = false; // for killing the infinite loop in the Status Thread
+    private boolean isTimerPaused = false; // for indicating whether the Timer is paused (to distinguish it from Timer
+    // being shut down
+    private static boolean hasSoundTriggered = false; // for indicating whether a sound has just been played
 
 
     // METHODS
@@ -19,7 +39,7 @@ public class SequenceTimer {
     // *** Utility Method(s)
     @Override
     public String toString() {
-
+        return "";
     }
 
     // MAIN METHOD FOR TESTING
