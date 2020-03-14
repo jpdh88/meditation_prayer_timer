a0955904d76374d8830a8d7b8ed94c0389db55a4
jpdh1988 <54423825+jpdh88@users.noreply.github.com>
2020-03-14 16:49:50 -0400

    Starts coding the GUI: background, sequence list view

--- java/src/GUI.java
+++ java/src/GUI.java
@@ -1,5 +1,11 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -19,6 +25,31 @@ public class GUI extends Application {
     private final double WINDOW_H = 732;
     private final double MARGIN = 10;
     private final double PADDING = 5;
+    //  - Instance variable(s)
+    Sequence sequence = new Sequence();
+    //  - Graphical components
+    //      *** background
+    Canvas canvasBG;
+    GraphicsContext gcBG;
+    //      *** ListView to display sequence
+    ListView<Interval> lvInterval;
+
+    // METHODS
+    //  - Helper methods
+    //  - Graphic methods
+    //      *** draw the ListView
+    public void drawLVInterval() {
+        // populate the list view with the sequence array
+        lvInterval.setItems(FXCollections.observableArrayList(sequence.getSequenceArray()));
+
+        // set the attributes
+        lvInterval.setPrefWidth(WINDOW_W - MARGIN * 2);
+        lvInterval.setPrefHeight(WINDOW_H * 0.07);
+        lvInterval.setOrientation(Orientation.HORIZONTAL);
+
+        lvInterval.relocate(MARGIN, WINDOW_H * 0.64);
+    }
+
 
     // TODO: Instance Variables for View Components and Model
     // TODO: Private Event Handlers and Helper Methods
@@ -35,14 +66,23 @@ public class GUI extends Application {
         Scene scene = new Scene(root, WINDOW_W, WINDOW_H); // size of the window
         stage.setTitle("Meditation and Prayer Timer"); // set the window title here
         stage.setScene(scene);
-        // TODO: Add your GUI-building code here
 
-        // 1. Create the model
-        // 2. Create the GUI components
-        // 3. Add components to the root
-        // 4. Configure the components (colors, fonts, size, location)
-        // 5. Add Event Handlers and do final setup
-        // 6. Show the stage
+        // TODO: 1. Create the model
+        // TODO: 2. Create the GUI components
+        // Set up the background
+        canvasBG = new Canvas(WINDOW_W, WINDOW_H);
+        gcBG = canvasBG.getGraphicsContext2D();
+        gcBG.drawImage(new Image("resources/images/background.png"), 0, 0, WINDOW_W, WINDOW_H);
+        // Set up the Interval Horizontal ListBox
+        lvInterval = new ListView<Interval>();
+        drawLVInterval();
+
+        // TODO: 3. Add components to the root
+        root.getChildren().addAll(canvasBG, lvInterval);
+
+        // TODO: 4. Configure the components (colors, fonts, size, location)
+        // TODO: 5. Add Event Handlers and do final setup
+        // TODO: 6. Show the stage
         stage.show();
     }
 
